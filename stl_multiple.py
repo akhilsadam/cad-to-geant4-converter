d20988a3f10a3a1061114c2e9edc55ed3f5368c5
Akhil Sadam <sadam.akhil@gmail.com>
2020-07-29 19:28:01 -0500

    Finish stl_multiple.py

--- stl_multiple.py
+++ stl_multiple.py
@@ -1,8 +1,14 @@
 #@ for multiple stl files (using folder name)
-#@ input:   python stl_multiple.py  relativePathToStlFolder/*.stl
+#@ input:   python stl_multiple.py <outputName> <pathToStlFolder/*.stl>
 
-import glob
+import os
 import sys
+import glob
 
-fileList = glob.glob(sys.argv[1])
-print(fileList)
+fileList = glob.glob(sys.argv[2])
+#print(fileList)
+strlist=""
+for i in fileList:
+    strlist += i + " "
+#print(strlist)
+os.system('cmd /c "python stl_gdml.py  "'+sys.argv[1]+" "+strlist)
